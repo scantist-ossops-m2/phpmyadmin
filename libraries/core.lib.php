e4c582800c8f47c606e0b56df635c437336bc587
jiongle1 <jiongle@scantist.com>
2024-04-04 22:53:53 +0800

    commit patch 4114228

--- libraries/core.lib.php
+++ libraries/core.lib.php
@@ -854,6 +854,7 @@ function PMA_isAllowedDomain($url)
         $_SERVER['SERVER_NAME'],
         /* phpMyAdmin domains */
         'wiki.phpmyadmin.net', 'www.phpmyadmin.net', 'phpmyadmin.net',
+        'demo.phpmyadmin.net',
         'docs.phpmyadmin.net',
         /* mysql.com domains */
         'dev.mysql.com','bugs.mysql.com',
